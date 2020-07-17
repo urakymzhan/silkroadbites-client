18d4e31ea39d69770da5e50aee329d61e2b05533
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-07-16 23:09:39 -0700

    🔥

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -7,7 +7,7 @@ export default function MyFiles({ data }) {
   return (
     <Layout>
       <div>
-        <h1>My Site's Files</h1>
+        <h1>These are my project files:</h1>
         <table>
           <thead>
             <tr>
